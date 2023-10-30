c288938259cf7824f09fe529a4054eea7ef9eade
Jakob Löhrer <jakob@pomino.ch>
2023-10-30 22:50:40 +0100

    add - components : add navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [selectedTab, setSelectedTab] = useState("Home");
+  const [selectedLang, setSelectedLang] = useState("EN");
+
+  const tabs = [
+    { label: "Home", id: 1 },
+    { label: "About", id: 2 },
+    { label: "Contact", id: 3 },
+    { label: "Projects", id: 4 },
+  ];
+
+  const languages = [
+    { label: "EN", id: 5 },
+    { label: "DE", id: 6 },
+  ];
+
+  return (
+    <div className="navbar">
+      <div className="tabs selection ">
+        {tabs.map((tab) => (
+          <div
+            key={tab.id}
+            className={`text-wrapper ${selectedTab === tab.label ? "bold" : ""}`}
+            onClick={() => setSelectedTab(tab.label)}
+          >
+            {tab.label}
+          </div>
+        ))}
+      </div>
+      <div className="languages selection">
+        {languages.map((lang) => (
+          <div             onClick={() => setSelectedLang(lang.label)}
+           key={lang.id} className={`text-wrapper ${selectedLang === lang.label ? "bold" : ""}`}>
+            {lang.label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
