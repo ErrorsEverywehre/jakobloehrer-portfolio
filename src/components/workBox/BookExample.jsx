b0219889f85e1a0edd8fe55d38b5f21357670fce
Jakob Löhrer <jakob@pomino.ch>
2023-11-14 16:14:38 +0100

    enhance - exampleBook : refactor

--- src/components/workBox/BookExample.jsx
+++ src/components/workBox/BookExample.jsx
@@ -6,6 +6,8 @@ const BookExample = () => {
   const [page1Flipped, setPage1Flipped] = useState(false);
   const [page2Flipped, setPage2Flipped] = useState(false);
   const [page3Flipped, setPage3Flipped] = useState(false);
+  const [bookOpened, setBookOpenend] = useState(false);
+  const [atStart, setAtStart] = useState(false);
 
   const numOfPapers = 3;
   const maxState = numOfPapers + 1;
@@ -22,30 +24,17 @@ const BookExample = () => {
     }
   };
 
-  // const openBook = () => {
-  //   book.style.transform = "translateX(50%)";
-  //   prevBtn.style.transform = "translateX(-180px)";
-  //   nextBtn.style.transform = "translateX(180px)";
-  // };
-
-  // const closeBook = (isFirstPage) => {
-  //   if (isFirstPage) {
-  //     book.style.transform = "translateX(0%)";
-  //   } else {
-  //     book.style.transform = "translateX(100%)";
-  //   }
-  //   prevBtn.style.transform = "translateX(0px)";
-  //   nextBtn.style.transform = "translateX(0px)";
-  // };
-
   useEffect(() => {
     switch (currentState) {
       case 1:
         setPage1Flipped(false);
         setPage2Flipped(false);
         setPage3Flipped(false);
+        setBookOpenend(false);
         break;
       case 2:
+        setBookOpenend(true);
+        setAtStart(true);
         setPage1Flipped(true);
         setPage2Flipped(false);
         setPage3Flipped(false);
@@ -56,6 +45,7 @@ const BookExample = () => {
         break;
       case 4:
         setPage3Flipped(true);
+        setBookOpenend(false);
         break;
       default:
         break;
@@ -67,41 +57,42 @@ const BookExample = () => {
       <button className="nav-btn" onClick={goPrevious}>
         left{" "}
       </button>
-
-      <div className="book">
-        <div className={`paper ${page1Flipped && "flipped"} p1`}>
-          <div className="front">
-            <div className="front-content">
-              <h1>Front 1</h1>
+      <div className="book-wrapper">
+        <div className={`book ${bookOpened ? "open" : "closed"}  ${atStart && "beginning"}`}>
+          <div className={`paper ${page1Flipped && "flipped"} p1`}>
+            <div className="front">
+              <div className="front-content">
+                <h1>Front 1</h1>
+              </div>
+            </div>
+            <div className="back">
+              <div className="back-content b1">
+                <h1>Back 1</h1>
+              </div>
             </div>
           </div>
-          <div className="back">
-            <div className="back-content b1">
-              <h1>Back 1</h1>
+          <div className={`paper ${page2Flipped && "flipped"} p2`}>
+            <div className="front">
+              <div className="front-content">
+                <h1>Front 2</h1>
+              </div>
+            </div>
+            <div className="back">
+              <div className="back-content b2">
+                <h1>Back 2</h1>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={`paper ${page2Flipped && "flipped"} p2`}>
-          <div className="front">
-            <div className="front-content">
-              <h1>Front 2</h1>
+          <div className={`paper ${page3Flipped && "flipped"} p3`}>
+            <div className="front">
+              <div className="front-content">
+                <h1>Front 3</h1>
+              </div>
             </div>
-          </div>
-          <div className="back">
-            <div className="back-content b2">
-              <h1>Back 2</h1>
-            </div>
-          </div>
-        </div>
-        <div className={`paper ${page3Flipped && "flipped"} p3`}>
-          <div className="front">
-            <div className="front-content">
-              <h1>Front 3</h1>
-            </div>
-          </div>
-          <div className="back">
-            <div className="back-content b3">
-              <h1>Back 3</h1>
+            <div className="back">
+              <div className="back-content b3">
+                <h1>Back 3</h1>
+              </div>
             </div>
           </div>
         </div>
